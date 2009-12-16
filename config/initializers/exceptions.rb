055372334ef4cab168df36e952ec85d50ca7e163
Ryan Wood <ryan.wood@gmail.com>
2009-12-15 23:16:53 -0500

    Added exception config. Set to EST time zone.

--- config/initializers/exceptions.rb
+++ config/initializers/exceptions.rb
@@ -1,1 +1,3 @@
 ExceptionNotifier.exception_recipients = %w(ryan.wood@gmail.com)
+ExceptionNotifier.sender_address = %("PrayerThread Error" <error@sourcescape.com>)
+ExceptionNotifier.email_prefix = "[PrayerThread] "
