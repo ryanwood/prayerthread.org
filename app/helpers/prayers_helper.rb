30ebd3ba3d2a29b339a4a77def79fc3b1bae8441
Ryan Wood <ryan.wood@gmail.com>
2009-12-21 15:46:37 -0500

    Refactored group display on prayer list/show

--- app/helpers/prayers_helper.rb
+++ app/helpers/prayers_helper.rb
@@ -1,8 +1,9 @@
 module PrayersHelper
   
   def group_list(prayer, link = true)
-    prayer.groups.map do |group|
+    groups = prayer.groups.map do |group|
       link ? link_to(group.name, group) : group.name
-    end.join(", ")
+    end
+    groups.empty? ? "no groups" : groups.join(', ')
   end
 end
