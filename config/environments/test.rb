8f2ed00d82a19018d7f66b9c8865e3a6a08d32fb
Ryan Wood <ryan.wood@gmail.com>
2010-01-05 10:57:07 -0500

    Reorder requires for testing (was causing problems)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,6 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
-config.gem 'mocha'
-config.gem 'factory_girl', :source => 'http://gemcutter.org'
 config.gem 'shoulda', :source => 'http://gemcutter.org'
+config.gem 'factory_girl', :source => 'http://gemcutter.org'
+config.gem 'mocha'
