999c1312aa575fd2bac60cb5c23aa96cbd5eee97
Ryan Wood <ryan.wood@gmail.com>
2010-01-11 09:04:40 -0500

    Filtered users from current group from autocomplete

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -1,7 +1,9 @@
 module InvitationsHelper
   
-  def format_related_users_for_autocomplete
-    related = current_user.related_users.map { |u| "'#{u.first_name} #{u.last_name} &lt;#{u.email}&gt;'" }
+  def format_related_users_for_autocomplete(group)
+    # Related users that are not in the the current group
+    related = current_user.related_users - group.users
+    related.map! { |u| "'#{u.first_name} #{u.last_name} &lt;#{u.email}&gt;'" }
     "var related_users = [ #{related.join(', ')} ];"
   end
   
