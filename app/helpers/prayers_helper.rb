61dac69c9e6ded1abd5e0cb82bb6097ef531d256
Ryan Wood <ryan.wood@gmail.com>
2010-01-12 12:37:44 -0500

    Remove groups user is not in

--- app/helpers/prayers_helper.rb
+++ app/helpers/prayers_helper.rb
@@ -1,9 +1,11 @@
 module PrayersHelper
   
   def group_list(prayer, link = true)
-    groups = prayer.groups.map do |group|
+    intersect = prayer.groups & current_user.groups
+    groups = intersect.map do |group|
       link ? link_to(group.name, group) : group.name
     end
     groups.empty? ? "no groups" : groups.join(', ')
   end
+  
 end
