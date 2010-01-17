4ff98e10fd04048bd397958fa784b5741a036e43
Ryan Wood <ryan.wood@gmail.com>
2010-01-17 00:01:41 -0500

    Added production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,3 +28,10 @@ config.action_view.cache_template_loading            = true
 # config.threadsafe!
 
 HOST = "prayerthread.org"
+
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.sendmail_settings = {
+    :location       => '/usr/sbin/sendmail',
+    :arguments      => "-i -t -r donotreply@prayerthread.org"
+}
+
