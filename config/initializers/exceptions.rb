394978579f6d4c44c35a5eb85d82870b44e4c740
Ryan Wood <ryan.wood@gmail.com>
2010-02-01 23:33:05 -0500

    Removed exception_notifier

--- config/initializers/exceptions.rb
+++
@@ -1,3 +0,0 @@
-ExceptionNotifier.exception_recipients = %w(ryan.wood@gmail.com)
-ExceptionNotifier.sender_address = %("PrayerThread Error" <error@sourcescape.com>)
-ExceptionNotifier.email_prefix = "[PrayerThread] "
