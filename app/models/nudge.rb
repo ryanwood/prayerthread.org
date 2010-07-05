bca8b0d46810db5545f070ae2a3ced0e7159905a
Ryan Wood <ryan.wood@gmail.com>
2010-07-05 11:30:51 -0400

    clean up
    * remove old nudge stuff
    * add alt tags to gravatars

--- app/models/nudge.rb
+++ app/models/nudge.rb
@@ -3,7 +3,7 @@ class Nudge < Activity
   after_create :notify_user
   
   def action_text
-    "nudged #{prayer.user.name} for an update on"
+    "nudged you for an update on"
   end
   
   # Only allow 1 nudge per week
