5744557976bca76880be4b2402cdf853d618a86a
Ryan Wood <ryan.wood@gmail.com>
2010-07-13 14:53:32 -0400

    prevent a nudge on the day of create/comment

--- app/models/nudge.rb
+++ app/models/nudge.rb
@@ -8,7 +8,7 @@ class Nudge < Activity
   
   # Only allow 1 nudge per week
   def self.allowed?(user, prayer)
-    !self.actor(user).regarding(prayer).rolling_week.exists?
+    prayer.thread_updated_at < 1.day.ago && !self.actor(user).regarding(prayer).rolling_week.exists?
   end
   
   protected
