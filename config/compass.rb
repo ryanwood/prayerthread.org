c444f3537eb854a3366af9d5c0cfd5b237c3bb34
Ryan Wood <ryan.wood@gmail.com>
2010-07-25 19:59:46 -0400

    update compass config

--- config/compass.rb
+++ config/compass.rb
@@ -5,13 +5,7 @@ project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/compiled"
-http_stylesheets_path = "/stylesheets"
 sass_dir = "app/stylesheets"
-images_dir = "public/images"
-http_images_path = "/images"
-javascripts_dir = "public/javascripts"
-http_javascripts_path = "/javascripts"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-line_comments = false
