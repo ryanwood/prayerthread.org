5ba225018ff7cc0a59c86c8cc81dd04e3be3d94d
Ryan Wood <ryan.wood@gmail.com>
2010-09-26 20:58:17 -0400

    refactor js defaults, update rails.js

--- config/application.rb
+++ config/application.rb
@@ -30,13 +30,15 @@ module Prayerthread
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # JavaScript files you want as :defaults (application.js is always included).
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.4.2.min jquery-ui-1.8.custom.min jquery.autocomplete rails application)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password, :password_confirmation]
-
+    
     config.generators do |g|
       g.template_engine :haml
       g.test_framework :rspec, :fixture => false, :views => false
