42c267b54bbaf4919a6ef3a04c064fb3c86265de
Ryan Wood <ryan.wood@gmail.com>
2010-10-04 15:01:22 -0400

    order activity but created_at desc

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -10,15 +10,15 @@ class Activity < ActiveRecord::Base
   named_scope :regarding, lambda { |prayer| { :conditions => { :prayer_id => prayer } } }
   named_scope :actor, lambda { |user| { :conditions => { :user_id => user } } }
   
-  named_scope :today,      lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.midnight.utc, Time.zone.now.midnight.tomorrow.utc ] } }
-  named_scope :yesterday,  lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.midnight.yesterday.utc, Time.zone.now.midnight.utc ] } }
-  named_scope :this_week,  lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.beginning_of_week.utc , Time.zone.now.midnight.yesterday.utc ] } }
-  named_scope :last_week,  lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.beginning_of_week.utc - 7.days, Time.zone.now.beginning_of_week.utc ] } }
-  named_scope :this_month, lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.beginning_of_month.utc, Time.zone.now.beginning_of_week.utc - 7.days ] } }
-  named_scope :last_month, lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", (Time.zone.now.beginning_of_month.utc - 1.day).beginning_of_month.utc, Time.zone.now.beginning_of_month.utc ] } }
-  named_scope :older_than_last_week, lambda { { :conditions => [ "activities.created_at < ?", Time.zone.now.beginning_of_week.utc - 7.days ], :limit => 30 } }
+  named_scope :today,      lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.midnight.utc, Time.zone.now.midnight.tomorrow.utc ], :order => 'created_at desc' } }
+  named_scope :yesterday,  lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.midnight.yesterday.utc, Time.zone.now.midnight.utc ], :order => 'created_at desc' } }
+  named_scope :this_week,  lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.beginning_of_week.utc , Time.zone.now.midnight.yesterday.utc ], :order => 'created_at desc' } }
+  named_scope :last_week,  lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.beginning_of_week.utc - 7.days, Time.zone.now.beginning_of_week.utc ], :order => 'created_at desc' } }
+  named_scope :this_month, lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", Time.zone.now.beginning_of_month.utc, Time.zone.now.beginning_of_week.utc - 7.days ], :order => 'created_at desc' } }
+  named_scope :last_month, lambda { { :conditions => [ "activities.created_at >= ? AND activities.created_at < ?", (Time.zone.now.beginning_of_month.utc - 1.day).beginning_of_month.utc, Time.zone.now.beginning_of_month.utc ], :order => 'created_at desc' } }
+  named_scope :older_than_last_week, lambda { { :conditions => [ "activities.created_at < ?", Time.zone.now.beginning_of_week.utc - 7.days ], :limit => 30, :order => 'created_at desc' } }
   
-  named_scope :rolling_week,  lambda { { :conditions => [ "activities.created_at >= ?", 7.days.ago ] } }
+  named_scope :rolling_week,  lambda { { :conditions => [ "activities.created_at >= ?", 7.days.ago ], :order => 'created_at desc' } }
   
   before_create :allowed?
   
