411be5b95c8ca03d97ffd1d4981ba899be27b3c0
Ryan Wood <ryan.wood@gmail.com>
2010-10-08 19:54:59 -0400

    fix shared example double loading with autotest

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -6,18 +6,6 @@ describe NotificationMailer do
   let(:recipient) { User.make!(:confirmed, :first_name => "Cindy", :last_name => "Johnson", :email => "cindy@example.org" ) }
   let(:prayer) { Prayer.make!(:title => "My Prayer", :body => "Please pray for me.", :user => owner) }
   
-  shared_examples_for "any notification" do
-    it "is to the recipient" do
-      mail.to.should eq([recipient.email])
-    end
-    it "is from prayerthread.org" do
-      mail.from.should eq(["notifications@prayerthread.org"])
-    end
-    it "includes the link to the prayer" do
-      mail.body.encoded.should match(prayer_url(prayer))
-    end
-  end
-  
   describe "prayer_created" do
     let(:mail) { NotificationMailer.prayer_created(recipient, prayer) }
 
